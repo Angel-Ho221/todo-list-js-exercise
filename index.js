d23a95b58a06cc8714db5b932f24459bfdbef569
Angel <angel.ho@ryerson.ca>
2020-07-31 21:17:19 +0000

    index1

--- index.js
+++ index.js
@@ -21,3 +21,5 @@ const tasks = [task1, task2];
 task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
 task1.logState(); // Clean Cat Litter has been completed
+
+// i can't do this fork and clone again...
